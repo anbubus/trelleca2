0dec6cce08e68192898697274e5d91471ada2a44
anbubus <kinhentosyenis@gmail.com>
2023-10-15 18:42:43 -0300

    almost done

--- trelleca/view.py
+++ trelleca/view.py
@@ -12,46 +12,62 @@ tasks = []
 def home():
     
     user = users.find_one({"_id": current_user.id})
-    tasks = user["tasks"]
+    tasks = []
+    for task in user["tasks"]:
+        task["_id"] = str(task["_id"])
+        task["tittle"] = task["titlle"]
+        task["date"] = task["date"].strftime("%b %d %Y %H:%M:%S")
+        task["completed"] = task["completed"]
+        tasks.append(task)
     return render_template('home.html', tasks = tasks)
 
 @views.route('/add-tasks', methods=['GET', 'POST'])
 @login_required
 def add_task():
     if request.method == 'POST':
-        task = request.form.get('task-description')
+        description = request.form.get('task-description')
         titlle = request.form.get('task-titlle')
-        if len(task)<1:
-            flash("pica curta irmao", category='error')
+        if len(description)<1:
+            flash("Empty quote.", category='error')
         else:
-            tasks = [{
+            task = {
+                "_id": ObjectId(),
                 "titlle": titlle,
-                "description": task,
-                "date": datetime.utcnow()}]
+                "description": description,
+                "date": datetime.utcnow(),
+                "completed": False}
             
-            users.update_one({"_id":current_user.id}, {"$set": {"tasks": tasks}})
+            users.update_one({"_id":current_user.id}, {"$push": {"tasks": task}})
+            return redirect(url_for('views.home'))
+    
+    return render_template('add-task.html')
             
-    return redirect(url_for('views.home'))
+    
 
-@views.route('/edit_task/<id>', methods=['GET', 'POST'])
+@views.route('/edit-task/<id>', methods=['GET', 'POST'])
 def edit_task(id):
-    if request.method == 'POST':
-        task = request.form.get('task-description')
-        titlle = request.form.get('task-titlle')
-        if len(task)<1:
-            flash("pica curta irmao", category='error')
-        else:
-            n_tasks = [{
-                "titlle": titlle,
-                "description": task,
-                "date": datetime.utcnow()}]
-            
-            users.update_one({"_id":current_user.id, "tasks._id":ObjectId(id)}, {"$set": {"tasks": n_tasks}})
+    if request.method == "POST":
+        description = request.form.get('task-description')
+        titlle = request.form.get("task-titlle")
+        if len(description)<1:
+            flash("Empty quote.", category='error')
+        else:    
+            users.update_one({"_id":current_user.id, "tasks._id":ObjectId(id)}, {"$set": {"tasks.$.titlle": titlle, "tasks.$.description": description}})
+            return redirect('/')
+    else:
+        flash("Error, empty canva", category="error") 
+    return render_template('add-task.html')
 
 
-@views.route('/delete_task/<id>', methods=['GET', 'POST'])
+@views.route('/delete-task/<id>', methods=['GET', 'POST'])
 def delete_task(id):
-    users.update_one({"_id":current_user.id, "tasks._id":ObjectId(id)}, {"$pull": {"tasks._id": ObjectId(id)}})
+    users.update_one({"_id":current_user.id}, {"$pull": {"tasks":{"_id": ObjectId(id)}}})
     flash("Task deletada com sucesso", category="success")
     return redirect("/")
+
+@views.route('/update-task', methods=['GET', 'POST'])
+def update_task():
+    task_id = request.form.get('task_id')
+    users.update_one({"_id":current_user.id, "tasks._id":ObjectId(task_id)}, {"$set": {"tasks.$.completed": True}})
+    return redirect('/')
 
